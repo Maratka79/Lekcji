e29bd0402a54eeccb359f8f8496abc3c93386703
Maratka79 <gitgithub117@gmail.com>
2025-03-20 13:04:32 +0100

    Lekcja1

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 #Deklaracja klasy
 class Samochod:
     marka = None
@@ -29,3 +30,49 @@ bmw = fiat
 
 bmw.color = 'czarny'
 print(fiat.color)
+
+#importujemy moodul abc
+from abc import ABC, abstractmethod
+
+
+
+
+#Defininujemy klase abstakcyjną shape dziedzieczącą po ABC
+class Base(ABC):
+    #Defnijujemy metodę abstrakcyjną a
+    @abstractmethod
+    def area(self):
+     pass
+
+
+#definijujemy inną metodę abstrakcujną perimeter
+    @abstractmethod
+    def perimeter(self):
+        pass
+
+class Rectangle(Base):
+        # Defnijujemy metodę abstrakcyjną a
+
+
+        def __init__(self, length, width):
+
+            self.length = length
+            self.width = width
+
+        
+        def area(self):
+            return self.length +self.width
+
+        # definijujemy inną metodę abstrakcujną perimeter
+        
+        def perimeter(self):
+            return 2 + (self.length + self.width)
+
+
+
+
+rect = Rectangle(4, 5)
+
+print("Pole prostokąta", rect.area())
+print("Obwód prostokąta", rect.perimeter())
+
